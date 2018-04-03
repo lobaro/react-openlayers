b583a604a38c25192fd6ae1ceb2718cceedcfc9e
Keith Moss <keithamoss@gmail.com>
2018-04-03 13:53:41 +0800

    Tweaking where we put the pre-generated typings
    dts-generator --name react-openlayers --project . --out dist/index.d.ts
    
    - 'react-openlayers/index' -> 'react-openlayers'
    - 'Remove the React.createContext guff at the end

--- src/@types/react-openlayers.d.ts
+++ src/@types/react-openlayers.d.ts
@@ -431,23 +431,3 @@ declare module 'react-openlayers' {
 	export { Interactions, Layers, Overlays, Controls, layer, custom, control, interaction, Map, Overlay, Util };
 
 }
-declare module 'react-openlayers/src/@types/index' {
-	// Needed until @types/react supports the new React 16.3.0 Context API
-	// https://github.com/DefinitelyTyped/DefinitelyTyped/pull/24509
-
-	import * as React from "react"; module "react" {
-	  type Provider<T> = React.ComponentType<{
-	    value: T;
-	    children?: ReactNode;
-	  }>;
-	  type Consumer<T> = ComponentType<{
-	    children: (value: T) => ReactNode;
-	    unstable_observedBits?: number;
-	  }>;
-	  interface Context<T> {
-	    Provider: Provider<T>;
-	    Consumer: Consumer<T>;
-	  }
-	  function createContext<T>(defaultValue: T, calculateChangedBits?: (prev: T, next: T) => number): Context<T>;
-	}
-}
